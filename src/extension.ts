be58b07fd0156a76e67b232e77f8cb34ac82dd56
Aaron Bushnell <aaron.bushnell@gmail.com>
2017-11-01 23:57:14 -0400

    Add support for multiple line rebounds

--- src/extension.ts
+++ src/extension.ts
@@ -21,30 +21,47 @@ function reboundDelete() {
     const editor = window.activeTextEditor;
     const doc = editor.document;
     const start = window.activeTextEditor.selections[0].start;
-    let range;
+    let lines = [];
 
     if (start.line === 0 && start.character === 0) {
         return false;
     }
 
-    if (start.character === 0) {
-        range = new Range(
-            window.activeTextEditor.selections[0].start.line,
-            window.activeTextEditor.selections[0].start.character,
-            window.activeTextEditor.selections[0].start.line - 1,
-            999999999
-        );
-    } else {
-        range = new Range(
-            window.activeTextEditor.selections[0].start.line,
-            window.activeTextEditor.selections[0].start.character,
-            window.activeTextEditor.selections[0].start.line,
-            0
-        );
+    for (let i = 0; i < window.activeTextEditor.selections.length; i++) {
+        if (window.activeTextEditor.selections[i].start.character === 0) {
+            lines.push(
+                {
+                    startLine: window.activeTextEditor.selections[i].start.line,
+                    startCharacter: window.activeTextEditor.selections[i].start.character,
+                    endLine: window.activeTextEditor.selections[i].start.line - 1,
+                    endCharacter: 999999999
+                }
+            );
+        } else {
+            lines.push(
+                {
+                    startLine: window.activeTextEditor.selections[i].start.line,
+                    startCharacter: window.activeTextEditor.selections[i].start.character,
+                    endLine: window.activeTextEditor.selections[i].start.line ,
+                    endCharacter: 0
+                }
+            );
+        }
     }
 
     return editor.edit(editorBuilder => {
-        editorBuilder.delete(range);
+        let range;
+
+        for (let i = 0; i < lines.length; i++) {
+            range = new Range(
+                lines[i].startLine,
+                lines[i].startCharacter,
+                lines[i].endLine,
+                lines[i].endCharacter
+            );
+
+            editorBuilder.delete(range);
+        }
     });
 }
 
